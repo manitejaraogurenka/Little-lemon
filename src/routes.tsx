c65f468206eda437b45e42ced6ab70136adbe35b
Maniteja Gurenka <maniteja360@gmail.com>
2024-03-11 08:50:15 +0530

    fix Home page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,9 +7,9 @@ import ConfirmationPage from "./pages/ConfirmationPage";
 export default function () {
   return (
     <Routes>
-      <Route element={<Home/>} path="/"/>
-      <Route element={<Bookingpage/>} path="/bookings"/>
-      <Route element={<ConfirmationPage/>} path="/confirmation"/>
+      <Route element={<Home />} path="/Little-lemon" />
+      <Route element={<Bookingpage />} path="/bookings" />
+      <Route element={<ConfirmationPage />} path="/confirmation" />
     </Routes>
   );
-};
+}
